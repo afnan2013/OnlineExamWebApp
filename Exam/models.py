f222784e7019a1af42696aabb82d441987564ceb
kucse160235 <afnan1635@cseku.ac.bd>
2020-10-21 23:03:40 +0600

    Addition of model 'Questions' in the Exam app

--- Exam/models.py
+++ Exam/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Exam(models.Model):
-    Question = models.CharField(max_length=100)
-    option1 = models.CharField(max_length=100)
-    option2 = models.CharField(max_length=100)
-    option3 = models.CharField(max_length=100)
-    option4 = models.CharField(max_length=100)
-    option5 = models.CharField(max_length=100)
-    correct = models.CharField(max_length=100)
+class Questions(models.Model):
+    CAT_CHOICES = (
+    ('set1','Set1'),
+    ('set2','Set2'),
+    ('set3','Set3'),
+    ('set4','Set4')
+    )
+    question = models.CharField(max_length = 250)
+    optiona = models.CharField(max_length = 100)
+    optionb = models.CharField(max_length = 100)
+    optionc = models.CharField(max_length = 100)
+    optiond = models.CharField(max_length = 100)
+    answer = models.CharField(max_length = 100)
+    catagory = models.CharField(max_length=20, choices = CAT_CHOICES)
 
+    class Meta:
+        ordering = ('-catagory',)
+
+    def __str__(self):
+        return self.question
